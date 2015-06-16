e650469535d99818b11196a37e65f8baf5b6c26e
justin <justin.ryan.holmes@icloud.com>
2015-06-15 19:40:56 -0600

    add user login and logout feature tests

---
+++ spec/features/user_logout_test.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'user logout', type: :feature do
+  let(:user) { User.create(username: "Tom", password: "password") }
+
+  it 'existing user can logout' do
+    visit login_path
+    fill_in "Username", with: "Tom"
+    fill_in "Password", with: "password"
+    click_button "Login"
+    click_button "Logout"
+
+    expect(current_path).to eq(login_path)
+  end
+end
