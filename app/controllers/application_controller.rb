0f58315bb4fd6437b946be274f902b4f03e79126
Josh Cass <joshuacass@gmail.com>
2015-06-17 17:06:17 -0600

    moved methods from helper module to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :load_cart, :current_user?
+  helper_method :logged_in?, :current_user
 
   def load_cart
     @cart = Cart.new(session[:cart])
@@ -9,4 +10,13 @@ class ApplicationController < ActionController::Base
   def current_user?
     redirect_to login_path, notice: "Login or Sign up!" unless logged_in?
   end
+
+  def logged_in?
+    current_user != nil
+  end
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
 end
+
