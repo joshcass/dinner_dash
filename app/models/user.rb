9680b3027b1e6dbd11e0ec4c5da3c51410a88448
Josh Cass <joshuacass@gmail.com>
2015-06-17 14:29:30 -0600

    passing off to justing

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  validates :username, presence: true,
+                       uniqueness: true
+  validates :email, presence: true,
+                    uniqueness: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
+  validates :first_name, :last_name, presence: true
+  validates :display_name, length: { in: 2..32 }
+
+  validates :password, presence: true,
+                       confirmation: true
+  validates :password_confirmation, presence: true
+  enum role: %w(default admin)
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
   resources :items, only: [:index]
   resources :categories, only: [:show]
   resources :carts, only: [:create]
   put '/carts', to: 'carts#update'
   delete '/carts', to: "carts#destroy"
   resources :orders, only: [:new]
-  root 'welcome#index'
+  resources :users, only: [:new, :create, :show]
 end
