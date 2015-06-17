63898fe4338fb00b143da158c2915d0bd7b5d51c
Josh Cass <joshuacass@gmail.com>
2015-06-17 10:48:01 -0600

    layouts restructure

---
+++ app/assets/javascripts/foundation.alert.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  // find element(s) with the '.alert-box' class
+  var $alertBox = $('.alert-box');
+
+  // fade $alertBox out after 1 second (1000 ms)
+  $alertBox.delay(1000).fadeOut('slow');
+});
+
