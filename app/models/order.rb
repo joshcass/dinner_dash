628825d14118d84e628f559955831c00a40a3093
Josh Cass <joshuacass@gmail.com>
2015-06-16 19:31:09 -0600

    adding new files from adding carts

---
+++ app/models/order.rb
@@ -0,0 +1,10 @@
+class Order < ActiveRecord::Base
+  has_many :order_items
+  has_many :items, through: :order_items
+
+  def add_order_items(cart)
+    cart.contents.each do |item_id, quantity|
+      self.order_items.new(item_id: item_id.to_i, quantity: quantity)
+    end
+  end
+end
