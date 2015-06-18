827da3f4b0115560d8140c7d48bc38d72ef07020
RMCornell <zen.alchemist5280@gmail.com>
2015-06-18 15:33:44 -0600

    Remove Item from Cart Feature test working

---
+++ spec/features/ua_user_can_remove_items_from_cart_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+describe 'user is able to remove items from cart', type: :feature do
+  let(:item1) { Fabricate(:item) }
+  let(:item2) { Fabricate(:item) }
+  let(:category1) { Fabricate(:category) }
+  let(:category2) { Fabricate(:category) }
+
+  before(:each) do
+    item1.categories << category1
+    item2.categories << category2
+    visit root_path
+  end
+
+  after(:each) do
+    Fabrication.clear_definitions
+  end
+
+  #==== Add Items to Cart =====
+  it 'removes items from cart' do
+    click_link('SHOP')
+
+    expect(page).to have_content(item1.name)
+    expect(page).to have_content(item1.description)
+    expect(page).to have_content(item1.price)
+    first(:link, item1.name).click
+
+    find('#quantity', :match => :first).find(:xpath, 'option[2]').select_option
+    click_button('ADD TO CART', :match => :first)
+
+    expect(page).to have_content("You now have 1")
+    first(:link, item1.name).click
+
+    find('#quantity', :match => :first).find(:xpath, 'option[2]').select_option
+    click_button('ADD TO CART', :match => :first)
+
+    expect(page).to have_content("You now have 2")
+
+    click_link('cart_link')
+
+    within_table("cart_table") do
+      click_link("X")
+    end
+
+    expect(page).to have_content("Item removed from cart")
+  end
+
+end
