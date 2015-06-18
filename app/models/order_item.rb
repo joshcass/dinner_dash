4585a4832411f25944644a7494e90a9ec3d742d8
justin <justin.ryan.holmes@icloud.com>
2015-06-18 13:32:49 -0600

    update order_item spec

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,7 @@
 class OrderItem < ActiveRecord::Base
+  validates :quantity, presence: true,
+                       numericality: { greater_than: 0 }
+
   belongs_to :order
   belongs_to :item
 end
