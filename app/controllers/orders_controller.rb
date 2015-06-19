319ad78a4a9e23c38724d9a182eee4f3cf02ef51
justin <justin.ryan.holmes@icloud.com>
2015-06-18 18:33:32 -0600

    create orders and empty cart after order is completed

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,17 @@ class OrdersController < ApplicationController
   end
 
   def create
-    
+    @order = Order.new
+    @order.add_order_items(@cart)
+    @order.user_id = session[:user_id]
+
+    if @order.save
+      @cart.remove_all
+      redirect_to @order, notice: "Order Completed!"
+    else
+      flash.now[:errors] = @order.errors.full_messages.join(", ")
+      render :new
+    end
   end
 
   def show
