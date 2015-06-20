3068828003f9f76b66643b5c83629666341b409b
justin <justin.ryan.holmes@icloud.com>
2015-06-20 10:00:29 -0600

    update login spec

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
 describe 'user login', type: :feature do
-  let(:user) { User.create(username: "Tom", password: "password") }
+  let(:user) { Fabricate(:user) }
 
-  xit 'existing user can login' do
-    visit login_path
-    fill_in "Username", with: "Tom"
+  it 'existing user can login' do
+    visit root_path
+    click_on "LOGIN"
+    fill_in "Username", with: user.username
     fill_in "Password", with: "password"
     click_button "LOGIN"
 
-    expect(current_path).to eq(user_path(user))
+    expect(page).to have_content(user.first_name.upcase)
+    expect(page).to_not have_content("LOGIN")
   end
 end
