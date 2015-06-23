a23ddf1ff566ff05f0b282939a2e91ca7dfc6aa6
RMCornell <zen.alchemist5280@gmail.com>
2015-06-23 11:57:13 -0600

    Testing for Authorized User adding items to Cart complete.

--- spec/features/user_can_add_items_to_cart_spec.rb
+++ spec/features/user_can_add_items_to_cart_spec.rb
@@ -1,29 +1,27 @@
 require 'rails_helper'
 
-describe 'ua users is able to add items to cart', type: :feature do
+describe 'authorized user can add items to cart', type: :feature do
   let(:item1) { item = Fabricate.build(:item)
-                item.categories << category1
-                item.save
-                item}
+  item.categories << category1
+  item.save
+  item}
   let(:item2) { item = Fabricate.build(:item)
-                item.categories << category2
-                item.save
-                item}
+  item.categories << category2
+  item.save
+  item}
   let(:category1) { Fabricate(:category) }
   let(:category2) { Fabricate(:category) }
+
   let(:user) { Fabricate(:user) }
 
   before(:each) do
+    ApplicationController.any_instance.stubs(:current_user).returns(user)
     item1.categories << category1
     item2.categories << category2
     visit root_path
   end
 
-  after(:each) do
-    Fabrication.clear_definitions
-  end
-
-  it 'adds item to cart' do
+  it 'adds items to cart' do
     click_link('SHOP')
     expect(page).to have_content(item1.name)
     expect(page).to have_content(item1.description)
@@ -40,24 +38,5 @@ describe 'ua users is able to add items to cart', type: :feature do
     click_button('ADD TO CART', :match => :first)
 
     expect(page).to have_content("You now have 2")
-  end
-
-  it 'wont lose items after logging in' do
-    click_link('SHOP')
-    first(:link, item1.name).click
-
-    find('#quantity', :match => :first).find(:xpath, 'option[2]').select_option
-    click_button('ADD TO CART', :match => :first)
-
-    click_link("cart_link")
-    click_button('CHECKOUT')
-    expect(page).to have_content("Login or Sign up!")
-
-    fill_in "Username", with: user.username
-    fill_in "Password", with: "password"
-    click_button "LOGIN"
-    expect(page).to have_content(item1.name)
-    expect(page).to have_content(item1.description)
-    click_button('CHECKOUT')
   end
 end
