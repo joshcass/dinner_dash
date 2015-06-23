8f83bcdb06cb53d02a960ad33357742add03b527
RMCornell <zen.alchemist5280@gmail.com>
2015-06-23 12:26:52 -0600

    Testing for Authorized User Removing item from cart complete

---
+++ spec/features/user_can_remove_items_from_cart_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+describe 'user can remove items from cart', type: :feature do
+  let(:item1) { item = Fabricate.build(:item)
+  item.categories << category1
+  item.save
+  item}
+  let(:item2) { item = Fabricate.build(:item)
+  item.categories << category2
+  item.save
+  item}
+  let(:category1) { Fabricate(:category) }
+  let(:category2) { Fabricate(:category) }
+
+  let(:user) { Fabricate(:user) }
+
+  before(:each) do
+    ApplicationController.any_instance.stubs(:current_user).returns(user)
+    item1.categories << category1
+    item2.categories << category2
+    visit root_path
+  end
+
+  it 'removes items from cart' do
+    click_link('SHOP')
+
+    expect(page).to have_content(item1.name)
+    expect(page).to have_content(item1.description)
+    expect(page).to have_content(item1.price)
+    first(:link, item1.name).click
+
+    find('#quantity', :match => :first).find(:xpath, 'option[2]').select_option
+    click_button('ADD TO CART', :match => :first)
+
+    expect(page).to have_content("You now have 1")
+    first(:link, item1.name).click
+
+    find('#quantity', :match => :first).find(:xpath, 'option[2]').select_option
+    click_button('ADD TO CART', :match => :first)
+
+    expect(page).to have_content("You now have 2")
+
+    click_link('cart_link')
+
+    within_table("cart_table") do
+      click_link("X")
+    end
+
+    expect(page).to have_content("Item removed from cart")
+  end
+
+
+end
