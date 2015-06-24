7839fee3cfd73c50cf719b7768dd833be0c34cd6
Josh Cass <joshuacass@gmail.com>
2015-06-24 06:58:10 -0600

    last two user tests complete

--- spec/features/user_cannot_access_other_user_info_spec.rb
+++ spec/features/user_cannot_access_other_user_info_spec.rb
@@ -1,6 +1,17 @@
+require 'rails_helper'
 
+describe 'logged in user cannot access other users info', type: :feature do
+  let(:user_1) { Fabricate(:user) }
+  let(:user_2) { Fabricate(:user) }
 
+  before(:each) do
+    ApplicationController.any_instance.stubs(:current_user).returns(user_1)
+  end
 
-#Log in as User
-#Create two differnet users and attempt to access second user page through first user
+  it 'will not let logged in user access another users page' do
+    visit user_path(user_2)
+    expect(page).to have_content(user_1.first_name)
+    expect(page).not_to have_content(user_2.first_name)
+  end
 
+end
--- spec/features/user_cannot_view_admin_pages_spec.rb
+++ spec/features/user_cannot_view_admin_pages_spec.rb
@@ -1,3 +1,20 @@
-#create a user
-#log in as user
-#attempt to visit admin page
+require 'rails_helper'
+
+describe 'logged in user cannot view admin pages', type: :feature do
+  let(:user) { Fabricate(:user) }
+  let(:admin){ User.create(username: "admin", first_name: "admin", last_name: "admin", email: "admin@admin.com", password: "password", password_confirmation: "password", role: "admin")}
+
+  before(:each) do
+    ApplicationController.any_instance.stubs(:current_user).returns(user)
+  end
+
+  it 'will not let user view admin items route' do
+    visit admin_items_path
+    expect(page).to have_content("The page you were looking for doesn't exist")
+  end
+
+  it 'will not let user view admin user route' do
+    visit admin_user_path(admin)
+    expect(page).to have_content("The page you were looking for doesn't exist")
+  end
+end
